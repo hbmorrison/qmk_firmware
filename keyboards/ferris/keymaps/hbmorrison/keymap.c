f54a25508554be4609aac7c1665f52b83a5accef
Hannah Morrison <hannah@landhmorrison.scot>
2023-08-01 11:07:17 +0100

    Fix alt tabbing. Move scrolling to nav layer.

--- keyboards/ferris/keymaps/hbmorrison/keymap.c
+++ keyboards/ferris/keymaps/hbmorrison/keymap.c
@@ -14,14 +14,19 @@ enum my_keycodes {
     M_ESCQ,
     M_ESCW,
     M_ESCV,
-    M_EQLR
+    M_EQLR,
+    M_ALTT
 };
+
+// Stores state of M_ALTT macro - true if we are currently tabbing between
+// windows.
+static bool m_altt_pressed = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE_LAYER] = LAYOUT_split_3x5_2(
     KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_BSPC,
     KC_A, LGUI_T(KC_R), LALT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RALT_T(KC_E), RGUI_T(KC_I), KC_O,
-    KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, TO(SCUT_LAYER),
+    KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, OSL(SCUT_LAYER),
     OSM(MOD_LSFT), KC_SPC, KC_ENT, OSL(SYM_LAYER)
   ),
   [SYM_LAYER] = LAYOUT_split_3x5_2(
@@ -38,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [NAV_LAYER] = LAYOUT_split_3x5_2(
     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-    KC_TAB, LCTL(KC_TAB), LALT(KC_TAB), KC_BTN1, KC_BTN2, KC_WH_U, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+    KC_TAB, LCTL(KC_TAB), M_ALTT, KC_BTN1, KC_BTN2, KC_WH_U, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
     TO(FUNC_LAYER), KC_NO, M_DSKP, M_DSKN, M_ESCV, KC_WH_D, KC_NO, KC_PGDN, KC_PGUP, KC_END,
     TO(BASE_LAYER), KC_SPC, KC_ENT, KC_NO
   ),
@@ -51,7 +56,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (keycode != M_ALTT && m_altt_pressed) {
+    unregister_code(KC_LALT);
+    m_altt_pressed = false;
+  }
   switch (keycode) {
+    case M_ALTT:
+      if (record->event.pressed) {
+        if (!m_altt_pressed) {
+          register_code(KC_LALT);
+          m_altt_pressed = true;
+        }
+        tap_code(KC_TAB);
+      }
+      break;
     case M_ESCQ:
       if (record->event.pressed) {
         SEND_STRING(SS_TAP(X_ESC)":q!"SS_TAP(X_ENT));
@@ -101,63 +119,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    // Return to the base layer after a function key is pressed.
-    case KC_F1 ... KC_F12:
-      if (!record->event.pressed) { layer_move(BASE_LAYER); }
-      break;
-    // Return to the base later after space or enter have been pressed.
+    // Return to the base layer if space, enter or a function key is pressed.
     case KC_SPC:
     case KC_ENT:
+    case KC_F1 ... KC_F12:
       if (!record->event.pressed) { layer_move(BASE_LAYER); }
       break;
     // Cancel caps lock if escape is pressed.
     case KC_ESC:
       if (host_keyboard_led_state().caps_lock) { tap_code(KC_CAPS); }
       break;
-    // Return to the num layer if symbols in the func layer have been pressed.
+    // Return to the nav layer if symbols in the func layer have been pressed.
     case KC_ASTR:
     case KC_PLUS:
     case KC_MINS:
     case KC_EQL:
     case KC_DOT:
-      if (!record->event.pressed) {
-        if (IS_LAYER_ON(FUNC_LAYER)) {
-          layer_move(NAV_LAYER);
-        }
-      }
-      break;
-    // Slash appears in multiple layers and returns to multiple layers.
     case KC_SLSH:
       if (!record->event.pressed) {
         if (IS_LAYER_ON(FUNC_LAYER)) {
           layer_move(NAV_LAYER);
-        }
-        if (IS_LAYER_ON(SCUT_LAYER)) {
-          layer_move(BASE_LAYER);
-        }
-      }
-      break;
-    // Make most of the shortcut layer shortcuts behave like a one shots.
-    case KC_TAB:
-    case KC_CAPS:
-    case LCTL(KC_X):
-    case LCTL(KC_C):
-    case LCTL(KC_V):
-    case LCTL(KC_F):
-    case LCTL(KC_B):
-    case LSFT(LCTL(KC_SPC)):
-    case LSFT(LCTL(KC_C)):
-    case HYPR(KC_1):
-    case HYPR(KC_2):
-    case HYPR(KC_3):
-    case HYPR(KC_G):
-    case HYPR(KC_M):
-    case HYPR(KC_4):
-    case HYPR(KC_5):
-    case HYPR(KC_6):
-      if (!record->event.pressed) {
-        if (IS_LAYER_ON(SCUT_LAYER)) {
-          layer_move(BASE_LAYER);
         }
       }
       break;
