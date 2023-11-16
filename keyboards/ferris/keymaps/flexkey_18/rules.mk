ce1f2ba0e53ddcfbe3eaad2101f7e40dc098547d
Hannah Morrison <hannah@landhmorrison.scot>
2023-11-16 21:28:40 +0000

    Got test platform working

---
+++ keyboards/ferris/keymaps/flexkey_18/rules.mk
@@ -0,0 +1,7 @@
+USER_NAME := hbmorrison
+SRC += flexkey.c
+
+FK_TOP_OUTER_KEY = no
+FK_OUTER_KEYS = no
+FK_INNER_KEYS = no
+FK_THUMB_KEYS = no
---
+++ keyboards/ferris/keymaps/flexkey_20_4/rules.mk
@@ -0,0 +1,7 @@
+USER_NAME := hbmorrison
+SRC += flexkey.c
+
+FK_TOP_OUTER_KEY = no
+FK_OUTER_KEYS = 1
+FK_INNER_KEYS = no
+FK_THUMB_KEYS = 2
