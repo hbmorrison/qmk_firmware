6eb6eba3986a4cdcdcb99342f4d07a324623c665
Hannah Morrison <hannah@landhmorrison.scot>
2023-11-22 12:51:48 +0000

    Move to new 20 key layout

--- keyboards/planck/rev7/keymaps/flexkey/rules.mk
+++ keyboards/planck/rev7/keymaps/flexkey/rules.mk
@@ -2,6 +2,8 @@ USER_NAME := flexkey
 
 # Flexkey layout features.
 
+FK_TOP_OUTER_KEY = yes
 FK_MIDDLE_OUTER_KEY = yes
-FK_INNER_KEYS = top
+FK_BOTTOM_OUTER_KEY = yes
+FK_INNER_KEYS = all
 FK_TWO_THUMBKEYS = yes
